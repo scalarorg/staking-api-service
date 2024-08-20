109ccafc1cc51d924021ba17f37d01e8a56e3d19
Duc Huong <duchuong007@gmail.com>
2024-08-20 10:47:11 +0700

    feat: pass cors for dApp path

--- internal/api/middlewares/cors.go
+++ internal/api/middlewares/cors.go
@@ -10,6 +10,7 @@ import (
 const (
 	maxAge                    = 300
 	stakerDelegationCheckPath = "/v1/staker/delegation/check"
+	dAppPath				  = "/v1/dApp"
 	galxeOrigin               = "https://app.galxe.com"
 )
 
@@ -23,6 +24,18 @@ func CorsMiddleware(cfg *config.Config) func(http.Handler) http.Handler {
 				return cors.Options{
 					AllowedOrigins: []string{galxeOrigin},
 					AllowedMethods: []string{"GET", "OPTIONS", "POST"},
+					MaxAge:         maxAge,
+					// Below is a workaround to allow the custom CORS header to be set.
+					// i.e OPTIONS will be manually injected into `Access-Control-Allow-Methods` header
+					OptionsPassthrough: true,
+				}
+			}
+			if r.URL.Path == dAppPath {
+				// Return CORS options specific to this route
+				return cors.Options{
+					AllowedOrigins: cfg.Server.AllowedOrigins,
+					AllowedMethods: []string{"GET", "OPTIONS", "POST, PUT, DELETE, PATCH"},
+					AllowedHeaders: []string{"Content-Type", "Authorization"},
 					MaxAge:         maxAge,
 					// Below is a workaround to allow the custom CORS header to be set.
 					// i.e OPTIONS will be manually injected into `Access-Control-Allow-Methods` header
@@ -48,6 +61,16 @@ func CorsMiddleware(cfg *config.Config) func(http.Handler) http.Handler {
 			if r.URL.Path == stakerDelegationCheckPath {
 				w.Header().Set("Access-Control-Allow-Origin", galxeOrigin)
 				w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS, POST")
+				if r.Method == http.MethodOptions {
+					// This is a preflight request, respond with 204 immediately
+					w.WriteHeader(204)
+				}
+			}
+
+			if r.URL.Path == dAppPath {
+				w.Header().Set("Access-Control-Allow-Origin", cfg.Server.AllowedOrigins[0])
+				w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS, POST, PUT, DELETE, PATCH")
+				w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 				if r.Method == http.MethodOptions {
 					// This is a preflight request, respond with 204 immediately
 					w.WriteHeader(204)
